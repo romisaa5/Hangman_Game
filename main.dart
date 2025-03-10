8f0be8919f18387289aa85bb1b3dc0743c7f367f
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-03-10 20:01:42 +0200

    add class hangman

--- main.dart
+++ main.dart
@@ -1,1 +1,19 @@
-void main(){}
+import 'dart:math';
+
+class Hangman {
+  final List<String> words = [
+    "flutter",
+    "dart",
+    "programming",
+    "hangman",
+    "developer"
+  ];
+  late String targetWord;
+  late List<String> hiddenWord;
+  int attempts = 6;
+  List<String> guessedLetters = [];
+    Hangman() {
+    targetWord = words[Random().nextInt(words.length)];
+    hiddenWord = List.filled(targetWord.length, "_");
+  }
+}
