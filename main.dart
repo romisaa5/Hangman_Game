6db990e265213a8c3c5349f57983cd78dacbd714
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-03-10 23:34:44 +0200

    add start & end game methods

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math';
 
 class Hangman {
@@ -12,8 +13,36 @@ class Hangman {
   late List<String> hiddenWord;
   int attempts = 6;
   List<String> guessedLetters = [];
-    Hangman() {
+  Hangman() {
     targetWord = words[Random().nextInt(words.length)];
     hiddenWord = List.filled(targetWord.length, "_");
   }
+
+  void startGame() {
+    print('Welcome To Hangman Game !');
+    print('Try to guess the Word ');
+    while (attempts > 0 && hiddenWord.contains('_')) {
+      displayGameState();
+      String guess = getUserInput();
+      checkGuess(guess);
+    }
+    endGame();
+  }
+
+  void endGame() {}
+  String getUserInput() {
+    stdout.write("Enter a letter: ");
+    String? input = stdin.readLineSync() ?? 'a'.toLowerCase();
+    if (input.isEmpty ||
+        input == null ||
+        input.length > 1 ||
+        guessedLetters.contains(input)) {
+      print("Invalid input. Please enter a single new letter.");
+      return getUserInput();
+    }
+    return input;
+  }
+
+  void displayGameState() {}
+  void checkGuess(String guess) {}
 }
